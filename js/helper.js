65839040c1524c9f9864a270fcf8fc6766aef1cb
Masoud Valafar <masoudvalafar@gmail.com>
2024-09-05 20:21:04 -0700

    Editing

--- js/helper.js
+++ js/helper.js
@@ -15,3 +15,26 @@ function addItem(item, elem) {
 
     document.getElementById(elem).appendChild(div);
 }
+
+function getMenus(activeMenu) {
+    const pages = [
+        ["Home", "./index.html"],
+        ["Education", "./education.html"],
+        ["Rate", "./rate.html"],
+        // ["Forgeries", "./forgery.html"],
+        ["Collection", "./collection.html"],
+        ["Resources", "./resources.html"],
+    ]
+
+    const div = document.createElement('div');
+    div.className = 'topnav';
+
+    for (let i = 0; i < pages.length; i++) {
+        if (activeMenu === pages[i][0]) {
+            div.innerHTML += ` <a href="` + pages[i][1] + `" class="activenav">` + pages[i][0] + `</a>`
+        } else {
+            div.innerHTML += ` <a href="` + pages[i][1] + `">` + pages[i][0] + `</a>`
+        }
+    }
+    document.getElementById("main-main").appendChild(div);
+}
