ea213fe214a6ea53874711763c1511471a9de20f
Masoud Valafar <masoudvalafar@gmail.com>
2025-05-10 11:49:34 -0700

    1914 provisional issue

--- items.js
+++ items.js
@@ -839,10 +839,15 @@ How is insurance calculated? 6 postage rate + 16ch registration + 6ch receipt + 
         description: 'TBD',
         id: '40227',
     },
-    "": {
+    "1914-provisional-1ch-1": {
         title: '',
-        description: '',
-        id: '',
+        description: 'TBD',
+        id: '40154',
+    },
+    "1914-provisional-3ch-1": {
+        title: '',
+        description: 'TBD',
+        id: '40094',
     },
 
 }
